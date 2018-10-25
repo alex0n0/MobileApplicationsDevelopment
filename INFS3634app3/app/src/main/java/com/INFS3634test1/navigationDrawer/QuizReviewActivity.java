7e4ae1dd82cdf40ab2a3df59ac2c6b1028526ab5
Jason <43053668+imJasonHou@users.noreply.github.com>
2018-10-25 18:37:31 +1100

    Update INFS3634app3/app/src/main/java/com/INFS3634test1/navigationDrawer/QuizReviewActivity.java
    importing widgets

--- INFS3634app3/app/src/main/java/com/INFS3634test1/navigationDrawer/QuizReviewActivity.java
+++ INFS3634app3/app/src/main/java/com/INFS3634test1/navigationDrawer/QuizReviewActivity.java
@@ -5,8 +5,13 @@ import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import java.util.List;
 
